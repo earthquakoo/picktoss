27356484c8ef1b895a133363f9a44afbd3716313
Earthquakoo <cream5343@gmail.com>
2024-03-24 18:51:36 +0900

    health-check update

--- src/main/java/com/picktoss/picktossserver/domain/auth/controller/AuthController.java
+++ src/main/java/com/picktoss/picktossserver/domain/auth/controller/AuthController.java
@@ -8,11 +8,9 @@ import com.picktoss.picktossserver.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +37,12 @@ public class AuthController {
         MemberInfoDto memberInfoDto = authService.transJsonToMemberInfoDto(decodeJson);
         JwtTokenDto jwtTokenDto = memberFacade.createMember(memberInfoDto);
         System.out.println("jwtTokenDto.getAccessToken() = " + jwtTokenDto.getAccessToken());
+    }
+
+    @GetMapping("/health-check")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck() {
+        return "I'm doing fine";
     }
 
 }
