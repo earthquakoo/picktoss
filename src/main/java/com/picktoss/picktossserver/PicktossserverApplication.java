c0c180b18c81f8ea7bef6bfcf9a6433d25edb8cb
Earthquakoo <cream5343@gmail.com>
2024-05-13 18:09:29 +0900

    🔧 fix: swagger access server url fix

--- src/main/java/com/picktoss/picktossserver/PicktossserverApplication.java
+++ src/main/java/com/picktoss/picktossserver/PicktossserverApplication.java
@@ -1,15 +1,17 @@
 package com.picktoss.picktossserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
-public class
-PicktossserverApplication {
+public class PicktossserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PicktossserverApplication.class, args);
--- src/main/java/com/picktoss/picktossserver/core/config/SwaggerConfig.java
+++ src/main/java/com/picktoss/picktossserver/core/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package com.picktoss.picktossserver.core.config;
 
 import com.fasterxml.classmate.TypeResolver;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +29,21 @@ import java.util.List;
 import java.util.function.Predicate;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://api.picktoss.com:{port}",
+                        description = "The production API server",
+                        variables = {
+                                @ServerVariable(name = "port", description = "Booking data",
+                                        defaultValue = "444")
+                        }
+                )
+        }
+)
 public class SwaggerConfig {
+
+    @Value("${picktoss.server_url")
+    public String picktossServerUrl;
 
     @Bean
     public OpenAPI openAPI() {
