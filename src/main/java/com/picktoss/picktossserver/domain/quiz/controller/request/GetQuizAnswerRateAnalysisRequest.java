5d7f6f7c75ed9102f1870f165de0e9c0b585a794
Earthquakoo <cream5343@gmail.com>
2024-07-03 23:30:27 +0900

    ♻️ refact: modify quiz answer rate analysis API response

--- src/main/java/com/picktoss/picktossserver/domain/quiz/controller/request/GetQuizAnswerRateAnalysisRequest.java
+++
@@ -1,8 +0,0 @@
-package com.picktoss.picktossserver.domain.quiz.controller.request;
-
-import lombok.Getter;
-
-@Getter
-public class GetQuizAnswerRateAnalysisRequest {
-    private Long categoryId;
-}
