8a9d046fb988ad74485e3c2dd13ac1079df39501
earthquake <cream5343@gmail.com>
2025-06-16 21:46:36 +0900

    ♻️ refact: 공개된 문서 상세조회 API 정보 수정

--- picktoss-server/src/main/java/com/picktoss/picktossserver/domain/notification/util/NotificationSendUtil.java
+++ picktoss-server/src/main/java/com/picktoss/picktossserver/domain/notification/util/NotificationSendUtil.java
@@ -1,12 +1,20 @@
 package com.picktoss.picktossserver.domain.notification.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.*;
 import com.picktoss.picktossserver.core.redis.RedisConstant;
 import com.picktoss.picktossserver.core.redis.RedisUtil;
+import com.picktoss.picktossserver.global.enums.notification.NotificationType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -19,6 +27,7 @@ public class NotificationSendUtil {
     public void sendNotificationByStarReward(Long memberId) {
         String title = "🌟 친구 초대 보상 도착";
         String content = "받은 별을 확인해 보세요!";
+
         Optional<String> optionalToken = redisUtil.getData(RedisConstant.REDIS_FCM_PREFIX, memberId.toString(), String.class);
         if (optionalToken.isEmpty()) {
             return ;
@@ -64,7 +73,54 @@ public class NotificationSendUtil {
         }
     }
 
-    public void sendNotificationByDailyQuizNotSolved(Long memberId) {
+    private void createNotificationForRedis(String notificationKey, String title, String content, LocalDateTime notificationSendTime, NotificationType notificationType) {
+        LocalDateTime createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
 
+        Map<String, Object> notificationData = Map.of(
+                "title", title,
+                "content", content,
+                "notificationSendTime", notificationSendTime,
+                "notificationType", notificationType.toString(),
+                "createdAt", createdAt,
+                "expiresAt", createdAt.plusWeeks(2)
+        );
+        redisUtil.setData(RedisConstant.REDIS_NOTIFICATION_PREFIX, notificationKey, notificationData, RedisConstant.REDIS_INVITE_LINK_EXPIRATION_DURATION_MILLIS);
+    }
+
+    private void addNotificationReceivedMemberData(Long memberId, String notificationKey) {
+        String memberIdKey = memberId.toString();
+
+        Optional<Map> existingNotificationData = redisUtil.getData(RedisConstant.REDIS_NOTIFICATION_PREFIX, notificationKey, Map.class);
+        if (existingNotificationData.isEmpty()) {
+            return ;
+        }
+
+        Optional<Map> existingNotificationReceivedMemberData = redisUtil.getData(RedisConstant.REDIS_NOTIFICATION_RECEIVED_MEMBER_PREFIX, memberIdKey, Map.class);
+
+        if (existingNotificationReceivedMemberData.isEmpty()) {
+            createNotificationReceivedMemberData(memberIdKey, notificationKey);
+        }
+
+        if (existingNotificationReceivedMemberData.isPresent()) {
+            Map notificationReceivedMemberData = existingNotificationReceivedMemberData.get();
+            Object notificationKeysObject = notificationReceivedMemberData.get("notificationKeys");
+
+            List<String> notificationKeys = new ObjectMapper().convertValue(notificationKeysObject, new TypeReference<List<String>>() {});
+            notificationKeys.add(notificationKey);
+
+            notificationReceivedMemberData.put("notificationKeys", notificationKeys);
+            redisUtil.setData(RedisConstant.REDIS_NOTIFICATION_RECEIVED_MEMBER_PREFIX, memberIdKey, notificationReceivedMemberData);
+        }
+    }
+
+    private void createNotificationReceivedMemberData(String memberIdKey, String notificationKey) {
+        List<String> notificationKeys = new ArrayList<>();
+        notificationKeys.add(notificationKey);
+
+        Map<String, Object> notificationReceivedMemberData = Map.of(
+                "notificationKeys", notificationKeys,
+                "createdAt", LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+        );
+        redisUtil.setData(RedisConstant.REDIS_NOTIFICATION_RECEIVED_MEMBER_PREFIX, memberIdKey, notificationReceivedMemberData);
     }
 }
