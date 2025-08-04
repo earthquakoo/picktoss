edb04f646ec2c0c98e0ca6fd7b241b29c7f55c89
earthquake <cream5343@gmail.com>
2025-08-04 23:15:46 +0900

    ♻️ refact: Public Document Search API 코드 수정

--- picktoss-server/src/main/java/com/picktoss/picktossserver/domain/document/service/PublicDocumentSearchService.java
+++ picktoss-server/src/main/java/com/picktoss/picktossserver/domain/document/service/PublicDocumentSearchService.java
@@ -10,6 +10,7 @@ import com.picktoss.picktossserver.domain.document.entity.DocumentBookmark;
 import com.picktoss.picktossserver.domain.document.repository.DocumentRepository;
 import com.picktoss.picktossserver.domain.member.entity.Member;
 import com.picktoss.picktossserver.domain.quiz.entity.Quiz;
+import com.picktoss.picktossserver.domain.quiz.repository.QuizRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,6 +27,7 @@ public class PublicDocumentSearchService {
 
     private final S3Provider s3Provider;
     private final DocumentRepository documentRepository;
+    private final QuizRepository quizRepository;
 
     public GetPublicDocumentsResponse findPublicDocuments(Long categoryId, Long memberId, int page, int pageSize) {
         if (pageSize < 1) {
@@ -158,6 +160,71 @@ public class PublicDocumentSearchService {
             }
         }
 
+        List<SearchDocumentsResponse.SearchDocumentsDto> quizzesByKeyword = findQuizzesByKeyword(keyword, memberId);
+        documentDtos.addAll(quizzesByKeyword);
+
         return new SearchDocumentsResponse(documentDtos);
+    }
+
+    private List<SearchDocumentsResponse.SearchDocumentsDto> findQuizzesByKeyword(String keyword, Long memberId) {
+        List<Quiz> quizzes = quizRepository.findAllByKeyword(keyword);
+        HashMap<Document, Quiz> documentQuizHashMap = new HashMap<>();
+
+        for (Quiz quiz : quizzes) {
+            Document document = quiz.getDocument();
+            if (!documentQuizHashMap.containsKey(document)) {
+                documentQuizHashMap.put(document, quiz);
+            }
+        }
+
+        List<SearchDocumentsResponse.SearchDocumentsDto> documentDtos = new ArrayList<>();
+        for (Document document : documentQuizHashMap.keySet()) {
+            Quiz quiz = documentQuizHashMap.get(document);
+            List<SearchDocumentsResponse.SearchDocumentsQuizDto> quizDtos = new ArrayList<>();
+
+            String content = s3Provider.findFile(document.getS3Key());
+
+            boolean isOwner = Objects.equals(memberId, document.getDirectory().getMember().getId());
+            boolean isBookmarked = Optional.ofNullable(document.getDocumentBookmarks())
+                    .orElse(Collections.emptySet())
+                    .stream()
+                    .anyMatch(bookmark -> Objects.equals(memberId, bookmark.getMember().getId()));
+
+            int bookmarkCount = 0;
+            if (document.getIsPublic()) {
+                Set<DocumentBookmark> documentBookmarkList = document.getDocumentBookmarks();
+                if (documentBookmarkList != null && !documentBookmarkList.isEmpty()) {
+                    bookmarkCount = documentBookmarkList.size();
+                }
+            }
+
+            SearchDocumentsResponse.SearchDocumentsQuizDto quizDto =
+                    SearchDocumentsResponse.SearchDocumentsQuizDto.builder()
+                            .question(quiz.getQuestion())
+                            .answer(quiz.getAnswer())
+                            .explanation(quiz.getExplanation())
+                            .build();
+
+            quizDtos.add(quizDto);
+
+            SearchDocumentsResponse.SearchDocumentsDto documentDto =
+                    SearchDocumentsResponse.SearchDocumentsDto.builder()
+                            .id(document.getId())
+                            .name(document.getName())
+                            .emoji(document.getEmoji())
+                            .content(content)
+                            .isOwner(isOwner)
+                            .isPublic(document.getIsPublic())
+                            .isBookmarked(isBookmarked)
+                            .tryCount(document.getTryCount())
+                            .bookmarkCount(bookmarkCount)
+                            .totalQuizCount(document.getQuizzes().size())
+                            .quizzes(quizDtos)
+                            .build();
+
+            documentDtos.add(documentDto);
+            break;
+        }
+        return documentDtos;
     }
 }
