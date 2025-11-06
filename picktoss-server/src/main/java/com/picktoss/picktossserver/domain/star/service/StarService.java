0b6fac768b126a0b13f1695dbe0bf5c06d58be0d
earthquake <cream5343@gmail.com>
2025-11-07 01:21:25 +0900

    ♻️ refact: 별 사용 내역 API 사용자 시간대 코드 적용

--- picktoss-server/src/main/java/com/picktoss/picktossserver/domain/star/service/StarService.java
+++ picktoss-server/src/main/java/com/picktoss/picktossserver/domain/star/service/StarService.java
@@ -3,12 +3,14 @@ package com.picktoss.picktossserver.domain.star.service;
 import com.picktoss.picktossserver.domain.star.dto.response.GetStarUsageHistories;
 import com.picktoss.picktossserver.domain.star.entity.StarHistory;
 import com.picktoss.picktossserver.domain.star.repository.StarHistoryRepository;
-import com.picktoss.picktossserver.domain.star.repository.StarRepository;
 import com.picktoss.picktossserver.global.enums.star.TransactionType;
+import com.picktoss.picktossserver.global.utils.DateTimeUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +20,9 @@ import java.util.List;
 public class StarService {
 
     private final StarHistoryRepository starHistoryRepository;
+    private final DateTimeUtil dateTimeUtil;
 
-    public GetStarUsageHistories findStarUsageHistories(Long memberId, TransactionType transactionType) {
+    public GetStarUsageHistories findStarUsageHistories(Long memberId, TransactionType transactionType, ZoneId memberZoneId) {
         List<StarHistory> starHistories;
 
         if (transactionType == null) {
@@ -30,11 +33,13 @@ public class StarService {
 
         List<GetStarUsageHistories.GetStarUsageHistoriesDto> starHistoriesDtos = new ArrayList<>();
         for (StarHistory starHistory : starHistories) {
+            LocalDateTime createdAt = dateTimeUtil.convertToMemberLocalDateTime(starHistory.getCreatedAt(), memberZoneId);
+
             GetStarUsageHistories.GetStarUsageHistoriesDto starHistoriesDto = GetStarUsageHistories.GetStarUsageHistoriesDto.builder()
                     .description(starHistory.getDescription())
                     .changeAmount(starHistory.getChangeAmount())
                     .transactionType(starHistory.getTransactionType())
-                    .createdAt(starHistory.getCreatedAt())
+                    .createdAt(createdAt)
                     .build();
 
             starHistoriesDtos.add(starHistoriesDto);
